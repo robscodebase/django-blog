06a28e2d50ed94d870c645cbf2442fd56beae313
Robert Reyna <robscodebase@gmail.com>
2018-03-27 06:06:44 -0400

    Added comment section to admin.py.

--- blog/admin.py
+++ blog/admin.py
@@ -11,3 +11,10 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
 admin.site.register(Post, PostAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+admin.site.register(Comment, CommentAdmin)
