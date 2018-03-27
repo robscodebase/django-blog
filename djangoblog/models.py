b332f2281da3161e1ea868e02bf1c38868e1406a
Robert Reyna <robscodebase@gmail.com>
2018-03-27 05:03:03 -0400

    Reconfigure models.py and added /blog.

--- djangoblog/models.py
+++ djangoblog/models.py
@@ -1,39 +1,16 @@
-from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+"""
+WSGI config for djangoblog project.
 
+It exposes the WSGI callable as a module-level variable named ``application``.
 
-class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(status='published')
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
 
+import os
 
-class Post(models.Model):
-    STATUS_CHOICES = (
-        ('draft', 'Draft'),
-        ('published', 'Published'),
-    )
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User, related_name='blog_posts')
-    body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+from django.core.wsgi import get_wsgi_application
 
-    objects = models.Manager() # The default manager.
-    published = PublishedManager() # The Dahl-specific manager.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoblog.settings")
 
-    class Meta:
-        ordering = ('-publish',)
-
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.publish.year,
-                                                 self.publish.strftime('%m'),
-                                                 self.publish.strftime('%d'),
-                                                 self.slug])
+application = get_wsgi_application()
