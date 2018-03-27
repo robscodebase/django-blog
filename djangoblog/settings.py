1329c732818a4c6a72814dc74e50a02202aa6e62
Robert Reyna <robscodebase@gmail.com>
2018-03-27 05:50:16 -0400

    Added demo email data to settings.py.

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'admin@gmail.com'
+EMAIL_HOST_PASSWORD = 'demopass'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
