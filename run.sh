90757216660c45d9c92189d0332edaa7a5c7eb9c
Robert Reyna <robscodebase@gmail.com>
2018-03-27 03:23:30 -0400

    Successful run of django with postres db.

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker stop django && docker rm django
 docker rmi django && docker rmi $(docker images -a | grep "none")
-docker-compose run web django-admin.py startproject djangoblog .
+#docker-compose run web django-admin.py startproject djangoblog .
+docker-compose up
 #docker logs -f django
