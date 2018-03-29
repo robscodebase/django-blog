d8b381980825b73d1a421aa3e867bd178ee5dcb6
Robert Reyna <robscodebase@gmail.com>
2018-03-29 09:15:06 -0400

    Added create superuser to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 set -e
 sleep 5
 python3 manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'demopass')" | python manage.py shell
 python3 manage.py runserver 0.0.0.0:8000
